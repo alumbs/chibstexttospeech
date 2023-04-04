c7c10111b415e911ddc17804d6be563e52d6e5dc
chibuzor alumba <chibuzor.alumba@trip-arc.com>
2023-04-04 10:25:57 -0400

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<app-speech-synthesis></app-speech-synthesis>',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'AngularTextToSpeech';
+  title = 'Day 23 Speech Synthesis';
+
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
 }
